ac9397c33bcfad906033bce22f60763bf3ce73db
tsc <thomas.schoeftner@smarter-ecommerce.com>
2020-05-14 16:11:07 +0200

    let specs wait for probe on sink before validating mocks

--- src/test/scala/com/example/app/ComplexEgressSpec.scala
+++ src/test/scala/com/example/app/ComplexEgressSpec.scala
@@ -26,63 +26,28 @@ class ComplexEgressSpec extends WordSpec with MustMatchers with ScalaFutures wit
     TestKit.shutdownActorSystem(actorSystem)
 
   val testData = List(Data("sepp", 3.14d), Data("hat", 2.718d), Data("gelbe", -273.15d), Data("Eier", 299.792d))
+  val expected = List("pi", "e", "absolute zero", "speed-o-light")
 
   "A ComplexEgress" should {
     "call synchronous mock method" in {
-      val testEgress = new ComplexEgress with MockedSyncTransformer
-      (testEgress.syncTransformer.transform _).expects(testData(0).id).returning("pi").once
-      (testEgress.syncTransformer.transform _).expects(testData(1).id).returning("e").once
-      (testEgress.syncTransformer.transform _).expects(testData(2).id).returning("absolute zero").once
-      (testEgress.syncTransformer.transform _).expects(testData(3).id).returning("speed-o-light").once
+      val testEgress = new ComplexEgress with MockedSyncTransformer with ProbedSink
+      (testEgress.syncTransformer.transform _).expects(testData(0).id).returning(expected(0)).once
+      (testEgress.syncTransformer.transform _).expects(testData(1).id).returning(expected(1)).once
+      (testEgress.syncTransformer.transform _).expects(testData(2).id).returning(expected(2)).once
+      (testEgress.syncTransformer.transform _).expects(testData(3).id).returning(expected(3)).once
 
       val testKit = AkkaStreamletTestKit(actorSystem, materializer)
       val src     = Source(testData)
       val in      = testKit.inletFromSource(testEgress.inlet, src)
-      //testKit.run(testEgress, in, () => {}) //FIXME - this would implicitly stop the streamlet execution, but mock validation ALWAYS fails (regardless of extra wait times) when using this
+
       val streamlet = testKit.run(testEgress, List(in), List.empty)
-      Await.result(streamlet.ready, 3 seconds)
+      testEgress.sinkProbe.fishForSpecificMessage(10 seconds) {
+        case completed @ ProbedSink.Completed => completed
+      }
+      streamlet.stop()
 
-      //FIXME - how to give the Streamlet adquate time to process all messages
-      //Await.result(streamlet.completed, 10 seconds) //FIXME - Streamlet never completes - why?
-
-      //FIXME - dirty hack to allow the Streamlet to do some actual processing! :(
-      Await.result(Future { Thread.sleep(1000) }, 2 seconds) // without this extra time, the mocks never get called and, hence, throw
-    }
-
-    "call asynchronous mock method" in {
-      val testEgress = new ComplexEgress with MockedAsyncTransformer
-      (testEgress.asyncTransformer.transformAsync _).expects(testData(0).id.reverse).returning(Future("pi")).once
-      (testEgress.asyncTransformer.transformAsync _).expects(testData(1).id.reverse).returning(Future("e")).once
-      (testEgress.asyncTransformer.transformAsync _).expects(testData(2).id.reverse).returning(Future("absolute zero")).once
-      (testEgress.asyncTransformer.transformAsync _).expects(testData(3).id.reverse).returning(Future("speed-o-light")).once
-
-      val testKit = AkkaStreamletTestKit(actorSystem, materializer)
-      val src     = Source(testData)
-      val in      = testKit.inletFromSource(testEgress.inlet, src)
-      //testKit.run(testEgress, in, () => {}) //FIXME - this would implicitly stop the streamlet execution, but mock validation ALWAYS fails (regardless of extra wait times) when using this
-      val streamlet = testKit.run(testEgress, List(in), List.empty)
-      Await.result(streamlet.ready, 3 seconds)
-
-      //FIXME - how to give the Streamlet adquate time to process all messages
-      //Await.result(streamlet.completed, 10 seconds) //FIXME - Streamlet never completes - why?
-
-      //FIXME - dirty hack to allow the Streamlet to do some actual processing! :(
-      Await.result(Future { Thread.sleep(1000) }, 2 seconds) // without this extra time, the mocks never get called and, hence, throw
-    }
-
-    "call function mock" in {
-      val testEgress = new ComplexEgress with MockedTransformFunc with ProbedSink
-      testEgress.transformFunc.expects(testData(0).id.reverse.toUpperCase).returning("pi").once
-      testEgress.transformFunc.expects(testData(1).id.reverse.toUpperCase).returning("e").once
-      testEgress.transformFunc.expects(testData(2).id.reverse.toUpperCase).returning("absolute zero").once
-      testEgress.transformFunc.expects(testData(3).id.reverse.toUpperCase).returning("speed-o-light").once
-
-      val testKit = AkkaStreamletTestKit(actorSystem, materializer)
-      val src     = Source(testData)
-      val in      = testKit.inletFromSource(testEgress.inlet, src)
-//      testKit.run(testEgress, in, () => {}) //FIXME - Why does this one work here, but not with object mocks (see above)?? - enabling it in the middle test will blow this one... very weird!!
-      val streamlet = testKit.run(testEgress, List(in), List.empty)
-      testEgress.sinkProbe.expectMsg(13 seconds, ProbedSink.Completed)
+//      //testKit.run(testEgress, in, () => {}) //FIXME - this would implicitly stop the streamlet execution, but mock validation ALWAYS fails (regardless of extra wait times) when using this
+//      val streamlet = testKit.run(testEgress, List(in), List.empty)
 //      Await.result(streamlet.ready, 3 seconds)
 //
 //      //FIXME - how to give the Streamlet adquate time to process all messages
@@ -90,6 +55,52 @@ class ComplexEgressSpec extends WordSpec with MustMatchers with ScalaFutures wit
 //
 //      //FIXME - dirty hack to allow the Streamlet to do some actual processing! :(
 //      Await.result(Future { Thread.sleep(1000) }, 2 seconds) // without this extra time, the mocks never get called and, hence, throw
+    }
+
+    "call asynchronous mock method" in {
+      val testEgress = new ComplexEgress with MockedAsyncTransformer with ProbedSink
+      (testEgress.asyncTransformer.transformAsync _).expects(testData(0).id.reverse).returning(Future(expected(0))).once
+      (testEgress.asyncTransformer.transformAsync _).expects(testData(1).id.reverse).returning(Future(expected(1))).once
+      (testEgress.asyncTransformer.transformAsync _).expects(testData(2).id.reverse).returning(Future(expected(2))).once
+      (testEgress.asyncTransformer.transformAsync _).expects(testData(3).id.reverse).returning(Future(expected(0))).once
+
+      val testKit = AkkaStreamletTestKit(actorSystem, materializer)
+      val src     = Source(testData)
+      val in      = testKit.inletFromSource(testEgress.inlet, src)
+
+      val streamlet = testKit.run(testEgress, List(in), List.empty)
+      testEgress.sinkProbe.fishForSpecificMessage(10 seconds) {
+        case completed @ ProbedSink.Completed => completed
+      }
+      streamlet.stop()
+
+//      //testKit.run(testEgress, in, () => {}) //FIXME - this would implicitly stop the streamlet execution, but mock validation ALWAYS fails (regardless of extra wait times) when using this
+//      val streamlet = testKit.run(testEgress, List(in), List.empty)
+//      Await.result(streamlet.ready, 3 seconds)
+//
+//      //FIXME - how to give the Streamlet adquate time to process all messages
+//      //Await.result(streamlet.completed, 10 seconds) //FIXME - Streamlet never completes - why?
+//
+//      //FIXME - dirty hack to allow the Streamlet to do some actual processing! :(
+//      Await.result(Future { Thread.sleep(1000) }, 2 seconds) // without this extra time, the mocks never get called and, hence, throw
+    }
+
+    "call function mock" in {
+      val testEgress = new ComplexEgress with MockedTransformFunc with ProbedSink
+      testEgress.transformFunc.expects(testData(0).id.reverse.toUpperCase).returning(expected(0)).once
+      testEgress.transformFunc.expects(testData(1).id.reverse.toUpperCase).returning(expected(1)).once
+      testEgress.transformFunc.expects(testData(2).id.reverse.toUpperCase).returning(expected(2)).once
+      testEgress.transformFunc.expects(testData(3).id.reverse.toUpperCase).returning(expected(3)).once
+
+      val testKit   = AkkaStreamletTestKit(actorSystem, materializer)
+      val src       = Source(testData)
+      val in        = testKit.inletFromSource(testEgress.inlet, src)
+      val streamlet = testKit.run(testEgress, List(in), List.empty)
+
+      testEgress.sinkProbe.fishForSpecificMessage(10 seconds) {
+        case completed @ ProbedSink.Completed => completed
+      }
+      streamlet.stop()
     }
 
     "call probed function (insted of mocked)" in {
@@ -104,7 +115,7 @@ class ComplexEgressSpec extends WordSpec with MustMatchers with ScalaFutures wit
       testEgress.probe.expectMsg(testData(3).id.reverse.toUpperCase)
       testEgress.probe.expectNoMessage()
       streamlet.stop()
-      Await.ready(streamlet.completed, 10 seconds) //actually not required because not mocks are wait for assertions in this test
+      Await.ready(streamlet.completed, 10 seconds) //actually not required because not mocks are waiting for assertions in this test
     }
   }
 
